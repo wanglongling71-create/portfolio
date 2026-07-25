d35ab914db813e6340ea68ed44c3099f7ea171c9
Wang Lingling <wanglingling71@gmail.com>
2026-07-25 14:22:51 +0800

    Fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://wanglongling71-create.github.io',
- base: '/',
+  base: '/portfolio',
 });
