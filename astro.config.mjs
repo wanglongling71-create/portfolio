8f6e1be976c488bc19636180227a24e7c81fa92c
Wang Lingling <wanglingling71@gmail.com>
2026-07-25 13:56:26 +0800

    Fix navigation links for base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wanglongling71-create.github.io',
-  base: '/portfolio',
+ base: '/',
 });
